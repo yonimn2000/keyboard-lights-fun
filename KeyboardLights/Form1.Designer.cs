144fd2dd7f7f9f4340b6e7666db49324970a386c
Jonathan <yonimn2000@gmail.com>
2017-07-30 14:59:05 -0400

    Add project files.

---
+++ KeyboardLights/Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace KeyboardLights
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.radioButton9 = new System.Windows.Forms.RadioButton();
+            this.radioButton10 = new System.Windows.Forms.RadioButton();
+            this.repeatsUD = new System.Windows.Forms.NumericUpDown();
+            this.speedUD = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.patternGroup = new System.Windows.Forms.GroupBox();
+            this.configGroup = new System.Windows.Forms.GroupBox();
+            this.continiousCB = new System.Windows.Forms.CheckBox();
+            this.startStopBTN = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.repeatsUD)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.speedUD)).BeginInit();
+            this.patternGroup.SuspendLayout();
+            this.configGroup.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(31, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 42);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(31, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 65);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(31, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(6, 88);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(31, 17);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(6, 111);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(31, 17);
+            this.radioButton5.TabIndex = 4;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "5";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(43, 19);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(31, 17);
+            this.radioButton6.TabIndex = 5;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "6";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(43, 42);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(31, 17);
+            this.radioButton7.TabIndex = 6;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "7";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Location = new System.Drawing.Point(43, 65);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(31, 17);
+            this.radioButton8.TabIndex = 7;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "8";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // radioButton9
+            // 
+            this.radioButton9.AutoSize = true;
+            this.radioButton9.Location = new System.Drawing.Point(43, 88);
+            this.radioButton9.Name = "radioButton9";
+            this.radioButton9.Size = new System.Drawing.Size(31, 17);
+            this.radioButton9.TabIndex = 8;
+            this.radioButton9.TabStop = true;
+            this.radioButton9.Text = "9";
+            this.radioButton9.UseVisualStyleBackColor = true;
+            // 
+            // radioButton10
+            // 
+            this.radioButton10.AutoSize = true;
+            this.radioButton10.Location = new System.Drawing.Point(43, 111);
+            this.radioButton10.Name = "radioButton10";
+            this.radioButton10.Size = new System.Drawing.Size(33, 17);
+            this.radioButton10.TabIndex = 9;
+            this.radioButton10.TabStop = true;
+            this.radioButton10.Text = "R";
+            this.radioButton10.UseVisualStyleBackColor = true;
+            // 
+            // repeatsUD
+            // 
+            this.repeatsUD.Location = new System.Drawing.Point(52, 19);
+            this.repeatsUD.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.repeatsUD.Name = "repeatsUD";
+            this.repeatsUD.Size = new System.Drawing.Size(39, 20);
+            this.repeatsUD.TabIndex = 11;
+            this.repeatsUD.Value = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            // 
+            // speedUD
+            // 
+            this.speedUD.Location = new System.Drawing.Point(43, 63);
+            this.speedUD.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.speedUD.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.speedUD.Name = "speedUD";
+            this.speedUD.Size = new System.Drawing.Size(48, 20);
+            this.speedUD.TabIndex = 12;
+            this.speedUD.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(6, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Repeats";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Speed";
+            // 
+            // patternGroup
+            // 
+            this.patternGroup.Controls.Add(this.radioButton1);
+            this.patternGroup.Controls.Add(this.radioButton2);
+            this.patternGroup.Controls.Add(this.radioButton3);
+            this.patternGroup.Controls.Add(this.radioButton4);
+            this.patternGroup.Controls.Add(this.radioButton5);
+            this.patternGroup.Controls.Add(this.radioButton6);
+            this.patternGroup.Controls.Add(this.radioButton10);
+            this.patternGroup.Controls.Add(this.radioButton7);
+            this.patternGroup.Controls.Add(this.radioButton9);
+            this.patternGroup.Controls.Add(this.radioButton8);
+            this.patternGroup.Location = new System.Drawing.Point(12, 12);
+            this.patternGroup.Name = "patternGroup";
+            this.patternGroup.Size = new System.Drawing.Size(78, 135);
+            this.patternGroup.TabIndex = 0;
+            this.patternGroup.TabStop = false;
+            this.patternGroup.Text = "Pattern";
+            // 
+            // configGroup
+            // 
+            this.configGroup.Controls.Add(this.repeatsUD);
+            this.configGroup.Controls.Add(this.speedUD);
+            this.configGroup.Controls.Add(this.label2);
+            this.configGroup.Controls.Add(this.label3);
+            this.configGroup.Controls.Add(this.continiousCB);
+            this.configGroup.Location = new System.Drawing.Point(96, 12);
+            this.configGroup.Name = "configGroup";
+            this.configGroup.Size = new System.Drawing.Size(99, 91);
+            this.configGroup.TabIndex = 15;
+            this.configGroup.TabStop = false;
+            this.configGroup.Text = "Configuration";
+            // 
+            // continiousCB
+            // 
+            this.continiousCB.AutoSize = true;
+            this.continiousCB.Location = new System.Drawing.Point(9, 40);
+            this.continiousCB.Name = "continiousCB";
+            this.continiousCB.Size = new System.Drawing.Size(79, 17);
+            this.continiousCB.TabIndex = 15;
+            this.continiousCB.Text = "Continuous";
+            this.continiousCB.UseVisualStyleBackColor = true;
+            this.continiousCB.CheckedChanged += new System.EventHandler(this.continiousCB_CheckedChanged);
+            // 
+            // startStopBTN
+            // 
+            this.startStopBTN.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startStopBTN.Location = new System.Drawing.Point(97, 110);
+            this.startStopBTN.Name = "startStopBTN";
+            this.startStopBTN.Size = new System.Drawing.Size(98, 37);
+            this.startStopBTN.TabIndex = 16;
+            this.startStopBTN.Text = "Start";
+            this.startStopBTN.UseVisualStyleBackColor = true;
+            this.startStopBTN.Click += new System.EventHandler(this.startStopBTN_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(206, 157);
+            this.Controls.Add(this.configGroup);
+            this.Controls.Add(this.startStopBTN);
+            this.Controls.Add(this.patternGroup);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Lights";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.repeatsUD)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.speedUD)).EndInit();
+            this.patternGroup.ResumeLayout(false);
+            this.patternGroup.PerformLayout();
+            this.configGroup.ResumeLayout(false);
+            this.configGroup.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.RadioButton radioButton9;
+        private System.Windows.Forms.RadioButton radioButton10;
+        private System.Windows.Forms.NumericUpDown repeatsUD;
+        private System.Windows.Forms.NumericUpDown speedUD;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox patternGroup;
+        private System.Windows.Forms.GroupBox configGroup;
+        private System.Windows.Forms.CheckBox continiousCB;
+        private System.Windows.Forms.Button startStopBTN;
+    }
+}
+
---
+++ KeyboardLights/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Collections.Generic;
+
+namespace KeyboardLights
+{
+    public partial class MainForm : Form
+    {
+        List<int>[] patterns = new List<int>[9];
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            patterns[0] = new List<int>() { 100, 010, 001, 010 };
+            patterns[1] = new List<int>() { 100, 010, 001 };
+            patterns[2] = new List<int>() { 001, 010, 100 };
+            patterns[3] = new List<int>() { 101, 010 };
+            patterns[4] = new List<int>() { 110, 011 };
+            patterns[5] = new List<int>() { 100, 010, 001, 000 };
+            patterns[6] = new List<int>() { 001, 010, 100, 000 };
+            patterns[7] = new List<int>() { 100, 110, 111, 011, 001, 000 };
+            patterns[8] = new List<int>() { 001, 011, 111, 110, 100, 000 };
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, ExactSpelling = true, CallingConvention = CallingConvention.Winapi)]
+        public static extern short GetKeyState(int keyCode);
+        bool[] keysStartState = new bool[3];
+        int lastState = 000;
+
+        private void startStopBTN_Click(object sender, EventArgs e)
+        {
+            if (startStopBTN.Text.Equals("Start"))
+            {
+                startStopBTN.Text = "Stop";
+                int pattern = 1;
+                RadioButton[] radios = { radioButton1, radioButton2, radioButton2, radioButton3, radioButton4, radioButton5, radioButton6, radioButton7, radioButton8, radioButton9, radioButton10 };
+                for (int i = 0; i < radios.Length; i++)
+                    if (radios[i].Checked)
+                    {
+                        pattern = i;
+                        break;
+                    }
+                bool[] isOn = { (((ushort)GetKeyState(0x90)) & 0xffff) != 0, (((ushort)GetKeyState(0x14)) & 0xffff) != 0, (((ushort)GetKeyState(0x91)) & 0xffff) != 0 };
+                keysStartState = isOn;
+                lastState = (keysStartState[0] ? 1 : 0) * 100 + (keysStartState[1] ? 1 : 0) * 10 + (keysStartState[2] ? 1 : 0);
+                patternGroup.Enabled = false;
+                configGroup.Enabled = false;
+                ShowPattern(pattern, (int)repeatsUD.Value, continiousCB.Checked);
+            }
+            else
+            {
+                SetKeys((keysStartState[0] ? 1 : 0) * 100 + (keysStartState[1] ? 1 : 0) * 10 + (keysStartState[2] ? 1 : 0), false);
+                startStopBTN.Text = "Start";
+                patternGroup.Enabled = true;
+                configGroup.Enabled = true;
+            }
+        }
+
+        public void ShowPattern(int pattern, int repeat, bool continious = false)
+        {
+            SetKeys(000);
+            for (int i = 0; i < repeat || continious; i++)
+                for (int j = 0; j < patterns[pattern].Count; j++)
+                    SetKeys(patterns[pattern][j]);
+            SetKeys(000);
+            startStopBTN.PerformClick();
+        }
+        
+        public void SetKeys(int keysState, bool sleep = true)
+        {
+            Key[] keys = new Key[3];
+            keys[0] = new Key(0x90);//Num
+            keys[1] = new Key(0x14);//Caps
+            keys[2] = new Key(0x91);//Scroll
+            bool[] isOn = { (lastState / 100 == 1), (lastState % 100 / 10 == 1), (lastState % 10 == 1) };
+            bool[] wantedKeyStates = { (keysState / 100 == 1), (keysState % 100 / 10 == 1), (keysState % 10 == 1) };
+            for (int i = 0; i < 3; i++)
+                if (isOn[i] != wantedKeyStates[i])
+                    keys[i].Toggle();
+            isOn = wantedKeyStates;
+            if (sleep)
+                System.Threading.Thread.Sleep((11 - (int)speedUD.Value) * 100);
+            lastState = keysState;
+        }
+
+        private void continiousCB_CheckedChanged(object sender, EventArgs e)
+        {
+            repeatsUD.Enabled = !continiousCB.Checked;
+        }
+    }
+
+    class Key
+    {
+        [DllImport("user32.dll")]
+        static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, UIntPtr dwExtraInfo);
+        public byte keyCode;
+
+        public Key(byte code)
+        {
+            keyCode = code;
+        }
+
+        public void Toggle()
+        {
+            keybd_event(keyCode, 0x45, 0x1, (UIntPtr)0);
+            keybd_event(keyCode, 0x45, 0x1 | 0x2, (UIntPtr)0);
+        }
+    }
+}
